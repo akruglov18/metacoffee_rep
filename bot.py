15a503a23be2b6549011ff56982a3c1edfdbb4d8
KimNikita <ghj710@mail.ru>
2021-12-16 22:29:03 +0300

    update structure

--- bot.py
+++ bot.py
@@ -13,7 +13,8 @@ orders = dict()
 order = {
     'coffee': None,
     'pattern': None,
-    'address': None
+    'address': None,
+    'isorder': None
 }
 
 commands = ['/help', '/order']
@@ -24,8 +25,9 @@ main_menu.row_width = 3
 main_menu.add(types.InlineKeyboardButton(text='Выбрать кофе', callback_data='1choose_coffee'))
 main_menu.add(types.InlineKeyboardButton(text='Выбрать узор', callback_data='2choose_pattern'))
 main_menu.add(types.InlineKeyboardButton(text='Ввести адрес доставки', callback_data='3input_address'))
-main_menu.add(types.InlineKeyboardButton(text='Оформить заказ', callback_data='4create_order'))
-main_menu.add(types.InlineKeyboardButton(text='Отменить заказ', callback_data='5cansel_order'))
+# добавить кнопку для указания времени доставки
+main_menu.add(types.InlineKeyboardButton(text='Оформить заказ', callback_data='5create_order'))
+main_menu.add(types.InlineKeyboardButton(text='Отменить заказ', callback_data='6cansel_order'))
 
 def normalize(text):
     text = text.lower()
@@ -35,7 +37,9 @@ def normalize(text):
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     global commands, messages, orders
-    # нужно предотвратить обработку когда есть кнопки
+    if message.from_user.id in orders.keys():
+        if orders[message.from_user.id]['isorder'] == True:
+            return
     text = normalize(message.text)
     if text in commands:
         parse_command(message)
@@ -57,6 +61,7 @@ def parse_command(command):
         bot.send_message(command.from_user.id, 'Распознаваемые команды: ' + str_commands + ' Распознаваемые сообщения: ' + str_messages)
     elif text == '/order':
         orders[command.from_user.id] = order
+        orders[command.from_user.id]['isorder'] = True
         keyboard = types.InlineKeyboardMarkup()
         key_yes = types.InlineKeyboardButton(text='Да', callback_data='0yes')
         key_no = types.InlineKeyboardButton(text='Нет', callback_data='0no')
@@ -64,6 +69,7 @@ def parse_command(command):
         keyboard.add(key_no)
         bot.send_message(command.from_user.id, text='Хотите сделать заказ?', reply_markup=keyboard)
 
+# убрать/закодировать павла + всякий текст
 def parse_message(message):
     text = normalize(message.text)
     if text == 'привет':
@@ -82,12 +88,14 @@ def callback_worker(call):
         if call.data[1:] == 'yes':
             bot.send_message(call.message.chat.id, text='Оформление заказа', reply_markup=main_menu)
         elif call.data[1:] == 'no':
+            orders.pop(call.from_user.id)
             bot.send_message(call.message.chat.id, 'Ну ладно :(')
     elif call.data[0] == '1':
         # выбираем кофе
         bot.edit_message_reply_markup(call.from_user.id, call.message.message_id)
         if call.data[1:] == 'choose_coffee':
             # создаем кнопки для выбора кофе
+            orders.pop(call.from_user.id)
             bot.send_message(call.message.chat.id, text='На данный момент кофе нету')
         elif call.data[1:] == 'coffee_1':
             # записываем выбранное кофе в заказ
@@ -103,9 +111,11 @@ def callback_worker(call):
     elif call.data[0] == '3':
         # вводим адрес
         # тут сложна, через кнопку не сделаешь
-        # + еще парсить адрес
+        # типо того: bot.register_next_handler(function name)
+        # вводи город, улицу и тд
         pass
-    elif call.data[0] == '4':
+    # добавить elif для call.data[0] == '4' для времени
+    elif call.data[0] == '5':
         # принимаем заказ
         if orders[call.message.chat.id]['coffee'] == None:
             bot.send_message(call.message.chat.id, 'Вы не выбрали кофе')
@@ -114,10 +124,12 @@ def callback_worker(call):
         elif orders[call.message.chat.id]['address'] == None:
             bot.send_message(call.message.chat.id, 'Вы не указали адрес доставки')
         else:
+            # вывести полную инфу о заказе
             bot.send_message(call.message.chat.id, 'Заказ принят')
             bot.edit_message_reply_markup(call.from_user.id, call.message.message_id)
-    elif call.data[0] == '5':
+    elif call.data[0] == '6':
         # отменяем заказ
+        orders.pop(call.from_user.id)
         bot.edit_message_reply_markup(call.from_user.id, call.message.message_id)
 
 bot.polling(none_stop=True, interval=0)
